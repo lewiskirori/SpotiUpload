12de484775228ec067533905e5e9ace49c0167e6
foreverblky <88181151+lewiskirori@users.noreply.github.com>
2026-09-01 13:10:17 +0300

    Fix typo in message

--- app/account/error.tsx
+++ app/account/error.tsx
@@ -7,7 +7,7 @@ const Error = () => {
         <Box className="h-full flex items-center justify-center">
             <div className="text-neutral-400 flex flex-col items-center">
                 <span style={{ fontSize: '1.2rem' }}>
-                    Oh sorry — it appears there’s been a hiccup!
+                    Oh sorry. It appears there’s been a hiccup!
                 </span>
                 <br />
                 <span style={{ fontSize: '0.9rem', marginTop: '0.5rem' }}>
